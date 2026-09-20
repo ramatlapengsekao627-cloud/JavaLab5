348fe8932c2a84c490b2b7874d9d4b75217475fe
Sekao R <ramatlapengsekao627@gmail.com>
2026-09-20 22:52:58 +0200

    First commit,Adding Question 1

---
+++ main/src/Book.java
@@ -0,0 +1,13 @@
+/*24019362
+ * Sekao Ramatlapeng
+ * Lab 5 Question 1*/
+
+/*Write a class called Book with three fields:
+ title (String), author (String), and price (double). Save it as Book.java.*/
+
+//just a class with basic fields
+public class Book {
+    String title;
+    String author;
+    double price;
+}
