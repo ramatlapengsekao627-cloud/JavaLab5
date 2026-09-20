611ca2ce47fadfcaf311083318d34dc12d32fc46
Sekao R <ramatlapengsekao627@gmail.com>
2026-09-20 23:14:35 +0200

    Added Question 4

---
+++ main/src/Question4.java
@@ -0,0 +1,16 @@
+/*24019362
+ * Sekao Ramatlapeng
+ * Lab 5 Question 4*/
+
+public class Question4 {
+    public static void main(String[] args) {
+        // create a Book object
+        Book book1 = new Book("The Hobbit", "J.R.R. Tolkien", 15.99);
+
+        // call getSummary() to get the combined String, then print it
+        System.out.println(book1.getSummary());
+    }
+}
+
+
+
